e6b362114a865532fe8c8f58bc871ffbe81f339c
ahyanarizky <ahyana.rizky@gmail.com>
2016-10-25 20:01:29 +0700

    success tweet api

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controller/controller.js')
+
+//view tweet made by user
+router.get('/twatt/mytweet', controller.mytweet)
+
+//view tweet on user timeline
+router.get('/twatt/timeline', controller.timeline)
+
+//view mentions on user
+router.get('/twatt/mentions', controller.mentions)
+
+//view search by query
+router.get('/twatt/search', controller.search)
+
+router.post('/twatt/update', controller.updateStatus)
+
+
+
+module.exports = router;
